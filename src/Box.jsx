1a819724951d6982efc44f7d35ce98096833d98c
Piotr Wołoszyn <piotr.woloszyn@onet.pl>
2025-07-23 13:00:23 +0200

    Pierwszy box

---
+++ src/Box.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+
+function Box() 
+{
+    return(
+        <>
+            <div>
+                <img src={reactLogo} className="logo" alt="Vite logo"/>
+            </div>
+        </>
+    )
+}
+
+export default Box
